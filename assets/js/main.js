cd936a8209589ac3f13e498ff89acfaba9d28307
Johnny Jiang <johnnyj2608@gmail.com>
2024-10-02 10:14:13 -0400

    Selection logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,37 @@
+function handleMealClick(menuType, menuItem) {
+    const panel = document.getElementById(`btn-${menuItem}`);
+
+    const selectedMenu = `selected${menuType}`;
+    panel.classList.toggle(selectedMenu);
+    document.querySelectorAll('.panel').forEach(btn => {
+        if (btn !== panel) {
+            btn.classList.remove(selectedMenu);
+        }
+    });
+    updateButtonState();
+}
+
 function handleNameClick(name) {
-    const listItems = document.querySelectorAll('#nameList li');
-    listItems.forEach(item => item.classList.remove('selected'));
-    event.target.classList.add('selected');
-    console.log('Selected:', name);
+    const listItem = document.getElementById(`name-${name.replace(/ /g, '_')}`);
+
+    listItem.classList.toggle('selected');
+    document.querySelectorAll('#nameList li').forEach(item => {
+        if (item !== listItem) {
+            item.classList.remove('selected');
+        }
+    });
+    updateButtonState();
+}
+
+function updateButtonState() {
+    const selectedName = document.querySelector('#nameList li.selected');
+    const selectedMeal = document.querySelector('.panel.selectedBreakfast, .panel.selectedLunch');
+
+    const submitButton = document.getElementById('submitButton');
+
+    if (selectedName && selectedMeal) {
+        submitButton.classList.remove('disabled');
+    } else {
+        submitButton.classList.add('disabled');
+    }
 }
