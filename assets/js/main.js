72e59f7dc06531184cab519952adf75cd9f1f11d
Johnny Jiang <johnnyj2608@gmail.com>
2024-10-02 11:04:49 -0400

    Retrieve selected items

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,6 @@ function handleNameClick(listItem) {
     updateButtonState();
 }
 
-
 function updateButtonState() {
     const selectedName = document.querySelector('#nameList li.selected');
     const selectedMeal = document.querySelector('.panel.selectedBreakfast, .panel.selectedLunch');
@@ -67,6 +66,25 @@ function resetSelection() {
 
 document.getElementById('submitButton').addEventListener('click', async function() {
     if (!this.classList.contains('disabled')) {
+        const selectedDay = document.querySelector('.selectedDay');
+        const weekday = selectedDay ? selectedDay.textContent : 'None';
+
+        const breakfastID = (document.querySelector('.selectedBreakfast')?.getAttribute('data-order')) || 'none';
+        const lunchID = (document.querySelector('.selectedLunch')?.getAttribute('data-order')) || 'none';
+
+        const breakfastName = (document.querySelector('.selectedBreakfast')?.getAttribute('data-text')) || 'none';
+        const lunchName = (document.querySelector('.selectedLunch')?.getAttribute('data-text')) || 'none';
+
+        const selectedName = document.querySelector('#nameList li.selected');
+        const memberName = selectedName ? selectedName.textContent : 'None';
+
+        console.log(`Weekday: ${weekday}`);
+        console.log(`Breakfast ID : ${breakfastID}`);
+        console.log(`Breakfast Name: ${breakfastName}`);
+        console.log(`Lunch ID: ${lunchID}`);
+        console.log(`Lunch Name: ${lunchName}`);
+        console.log(`Member Name: ${memberName}`);
+        
         resetSelection();
     }
 });
