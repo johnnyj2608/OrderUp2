95a88b99242dff4aff3900c45c679a1ce1e020e3
Johnny Jiang <johnnyj2608@gmail.com>
2024-10-26 12:58:36 -0400

    Day switching

--- assets/js/response.js
+++ assets/js/response.js
@@ -1,3 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const params = new URLSearchParams(window.location.search);
+    const selectedDay = params.get('day') || 0;
+    
+    if (selectedDay == 0) {
+        const todayCheckbox = document.getElementById('today-icon');
+        todayCheckbox.classList.add('fa-calendar-check');
+        todayCheckbox.classList.remove('fa-calendar');
+    } else {
+        const currentDayButton = document.querySelector(`.dayButton[data-day="${selectedDay}"]`);
+        currentDayButton.classList.add('selectedDay');
+    }
+});
+
+function handleDayClick(dayIndex) {
+    window.location.href = `?day=${dayIndex}`;
+}
+
 document.getElementById('backButton').addEventListener('click', async () => {
     window.location.href = '/';
 });
