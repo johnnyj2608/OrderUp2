931da282c287b3f9940838e66b50eafd86720ddb
Johnny Jiang <johnnyj2608@gmail.com>
2024-10-30 10:25:48 -0400

    History data into date:responses

--- routes/historyRoute.js
+++ routes/historyRoute.js
@@ -7,16 +7,57 @@ router.get('/history', async (req, res) => {
         const googleSheets = getGoogleSheets();
         const spreadsheet = await googleSheets.spreadsheets.values.get({
             spreadsheetId,
-            range: `History!A1:DW`,
+            range: `History!A1:ES`,
             valueRenderOption: 'FORMULA'
         });
         const data = spreadsheet.data.values;
 
-        res.render('history');
+        const dateNumbers = data[0].filter(cell => cell !== "");
+        const dates = dateNumbers.map(convertSerialToDate);
+
+        result = {};
+        for (let i = 2; i < data.length; i++) {
+            for (let j = 0; j < data[i].length; j += 5) {
+                const date = dates[j / 5]
+                if (!result[date]) {
+                    result[date] = [];
+                }
+
+                if( data[i][j] === '') {
+                    continue;
+                }
+
+                const name = data[i][j]
+                const breakfast = data[i][j+1]
+                const lunch = data[i][j+2]
+                const timestamp = data[i][j+3]
+
+                result[date].push({
+                    name: name,
+                    breakfast: breakfast,
+                    lunch: lunch,
+                    timestamp: timestamp
+                });
+            }
+        }
+
+        res.render('history', {result});
     } catch (error) {
         console.error("Error loading sheet data: ", error);
         res.status(500).send("Error loading sheet data");
     }
 });
+
+function convertSerialToDate(serial) {
+    const excelEpoch = new Date(Date.UTC(1899, 11, 30));
+    const dayInMilliseconds = 24 * 60 * 60 * 1000;
+    const date = new Date(excelEpoch.getTime() + serial * dayInMilliseconds);
+
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+    const day = date.getUTCDate().toString().padStart(2, '0');
+    const year = date.getUTCFullYear().toString().slice(-2);
+
+    return `${month}/${day}/${year}`;
+}
 
 module.exports = router;
