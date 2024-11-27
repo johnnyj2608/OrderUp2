866babd18a5d2483e3191f5e59faad81e18c95e4
Johnny Jiang <johnnyj2608@gmail.com>
2024-11-27 11:08:00 -0500

    Member list with new DB

--- routes/membersRoute.js
+++ routes/membersRoute.js
@@ -1,44 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
-const sheetsByWeekday = {
-    0: 'Today',
-    1: 'Mon',
-    2: 'Tue',
-    3: 'Wed',
-    4: 'Thu',
-    5: 'Fri',
-    6: 'Sat',
-};
+const { connectToDb } = require('../database/db');
 
 router.get('/members', async (req, res) => {
     try {
-        let selectedDay = req.query.day || 0;
-        let sheetName = sheetsByWeekday[selectedDay];
+        const client = await connectToDb();
 
-        const googleSheets = getGoogleSheets();
-        const spreadsheet = await googleSheets.spreadsheets.values.get({
-            spreadsheetId,
-            range: `${sheetName}!I4:L`,
-            valueRenderOption: 'FORMULA'
-        });
-        const data = spreadsheet.data.values;
-
-        const memberList = [];
-
-        data.forEach(row => {
-            const member = {
-                id: row[0],
-                name: row[1],
-                breakfast: row[2],
-                lunch: row[3],
-            };
-            memberList.push(member);
-        });
+        const query = 'SELECT * FROM members';
+        const result = await client.query(query);
+        const memberList = result.rows;
 
         res.render('members', { memberList });
     } catch (error) {
-        console.error("Error loading sheet data: ", error);
         res.status(500).send("Error loading sheet data");
     }
 });
