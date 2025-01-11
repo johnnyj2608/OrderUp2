203be76f0fd0e636f450cb18e25f9ec2260f2434
Johnny Jiang <johnnyj2608@gmail.com>
2025-01-11 10:36:40 -0500

    Locales for B & L types

--- assets/js/realTimeDB/rtMain.mjs
+++ assets/js/realTimeDB/rtMain.mjs
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-    
+
 const supabaseUrl = 'https://sxvnljeaffmiehlwhmkm.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4dm5samVhZmZtaWVobHdobWttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIxMTkxNDUsImV4cCI6MjA0NzY5NTE0NX0.jgsqFIQEO5EPpaPZVuXuLZhHXG_Axge6DSaRFLI3lxY';
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -17,10 +17,10 @@ const channel = supabase.channel('orders-submit')
             listItem.parentNode.removeChild(listItem);
         } else if (breakfast) {
             listItem.setAttribute('data-menu', 'L');
-            listItem.querySelector('.menu-type').innerText = 'L';
+            listItem.querySelector('.menu-type').innerText = typeL;
         } else if (lunch) {
             listItem.setAttribute('data-menu', 'B');
-            listItem.querySelector('.menu-type').innerText = 'B';
+            listItem.querySelector('.menu-type').innerText = typeB;
         }
     })
     .subscribe();
