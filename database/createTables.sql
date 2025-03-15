5d36908bd6e657b142ff78d29725a4d0e6f36dca
Johnny Jiang <johnnyj2608@gmail.com>
2025-03-15 10:19:51 -0400

    Index now varchar and max of 10

--- database/createTables.sql
+++ database/createTables.sql
@@ -16,7 +16,7 @@ CREATE TABLE menu (
 -- Create Members Table
 CREATE TABLE members (
   id SERIAL PRIMARY KEY,
-  index INT DEFAULT NULL,
+  index VARCHAR(10) UNIQUE NOT NULL,
   name VARCHAR(100) DEFAULT NULL,
   units INT DEFAULT 0,
   max INT DEFAULT 1
